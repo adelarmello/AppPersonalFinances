d5d87a71d69b7161c070d790703d985c2b95fceb
adelarmello <adelarmello8@gmail.com>
2020-02-07 00:24:29 -0300

    Página de relatórios

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {AppRegistry} from 'react-native';
 import App from './src';
 import Main from './src/Pages/Main';
 import NewEntry from './src/Pages/NewEntry';
+import Report from './src/Pages/Report';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => NewEntry);
+AppRegistry.registerComponent(appName, () => Report);
