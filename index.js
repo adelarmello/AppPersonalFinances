33040ae34225013eec4e3998ae3e2fa8a6a90940
adelarmello <adelarmello8@gmail.com>
2020-02-07 00:09:54 -0300

    Página de novos lançamentos/entradas

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ if(__DEV__) {
 import {AppRegistry} from 'react-native';
 import App from './src';
 import Main from './src/Pages/Main';
+import NewEntry from './src/Pages/NewEntry';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => NewEntry);
